d9b22f6793296646fff85a4e8f1bfc858b93c624
Spencer McIntyre <zeroSteiner@gmail.com>
2019-06-02 17:10:40 -0500

    Add support for datagram sockets in packet_parser

--- lib/rex/post/meterpreter/packet_parser.rb
+++ lib/rex/post/meterpreter/packet_parser.rb
@@ -31,11 +31,12 @@ class PacketParser
   #
   def recv(sock)
     raw = nil
-    if self.packet.raw_bytes_required > 0
-      while (raw = sock.read(self.packet.raw_bytes_required))
-        self.packet.add_raw(raw)
-        break if self.packet.raw_bytes_required == 0
-      end
+    if sock.type? == 'tcp'
+      recv_stream(sock)
+    elsif sock.type? == 'udp'
+      recv_datagram(sock)
+    else
+      raise ArgumentError('sock is not either tcp or udp')
     end
 
     if self.packet.raw_bytes_required > 0
@@ -53,6 +54,29 @@ class PacketParser
 
 protected
   attr_accessor :cipher, :packet    # :nodoc:
+
+  def recv_datagram(sock)
+    buffer = ''
+
+    while self.packet.raw_bytes_required > 0
+      if buffer.length < self.packet.raw_bytes_required
+        buffer << sock.read(65507)
+      end
+
+      raw = buffer[0..self.packet.raw_bytes_required - 1]
+      buffer = buffer[self.packet.raw_bytes_required..-1]
+      self.packet.add_raw(raw)
+    end
+  end
+
+  def recv_stream(sock)
+    if self.packet.raw_bytes_required > 0
+      while (raw = sock.read(self.packet.raw_bytes_required))
+        self.packet.add_raw(raw)
+        break if self.packet.raw_bytes_required == 0
+      end
+    end
+  end
 
 end
 
