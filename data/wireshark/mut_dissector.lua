b62e6adcf719e0eda05bbfb369d55a7e68eb3bb1
Spencer McIntyre <zeroSteiner@gmail.com>
2019-07-12 12:50:52 -0400

    Improve the MUT Wireshark dissector with fields

--- data/wireshark/mut_dissector.lua
+++ data/wireshark/mut_dissector.lua
@@ -14,23 +14,32 @@
 
 protocol = Proto("MUT_v1", "Metasploit UDP Transport v1")
 
+local FLAG_STRINGS = {[0] = "Not set", [1] = "Set"}
+
+-- See: https://www.wireshark.org/docs/wsdg_html_chunked/lua_module_Proto.html
+f_version  = ProtoField.uint8("mut_v1.version", "Version", base.DEC, nil, 0xf0)
+f_flag_psh = ProtoField.uint8("mut_v1.flag_psh", "PSH Flag", base.DEC, FLAG_STRINGS, 0x4)
+f_flag_ack = ProtoField.uint8("mut_v1.flag_ack", "ACK Flag", base.DEC, FLAG_STRINGS, 0x2)
+f_flag_syn = ProtoField.uint8("mut_v1.flag_syn", "SYN Flag", base.DEC, FLAG_STRINGS, 0x1)
+f_sequence = ProtoField.uint24("mut_v1.sequence", "Sequence", base.HEX)
+protocol.fields = { f_version, f_flag_psh, f_flag_ack, f_flag_syn, f_sequence }
+
 function protocol.dissector(buffer, pinfo, tree)
+    -- Check for the minimum buffer length required to process this frame
     if buffer:len() < 4 then return end
 
-    local header = buffer(0,1):uint()
-    local version = bit.rshift(bit.band(header, 0xf0), 4)
+    -- Check that the version number is 1 before proceeding
+    local version = bit.rshift(bit.band(buffer(0,1):uint(), 0xf0), 4)
     if version ~= 1 then return end
 
     pinfo.cols.protocol = protocol.name
+ 
     local subtree = tree:add(protocol, buffer(), protocol.description)
-
-    subtree:add(buffer(0,1), "Version: " .. tostring(version))
-
-    subtree:add(buffer(0,1), "Flag (PSH): " .. (bit.band(header, 0x4) == 0 and 'False' or 'True'))
-    subtree:add(buffer(0,1), "Flag (ACK): " .. (bit.band(header, 0x2) == 0 and 'False' or 'True'))
-    subtree:add(buffer(0,1), "Flag (SYN): " .. (bit.band(header, 0x1) == 0 and 'False' or 'True'))
-
-    subtree:add(buffer(1,3), "Sequence: " .. tostring(buffer(1,3):uint()))
+    subtree:add(f_version, buffer(0, 1))
+    subtree:add(f_flag_psh, buffer(0, 1))
+    subtree:add(f_flag_ack, buffer(0, 1))
+    subtree:add(f_flag_syn, buffer(0, 1))
+    subtree:add(f_sequence, buffer(1, 3))
 end
 
 -- Register the protocol to handle UDP port 4444
