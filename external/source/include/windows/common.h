d8228133376566770bdc4992009bd88bcd566602
Spencer McIntyre <zeroSteiner@gmail.com>
2022-02-17 15:36:05 -0500

    Initial backport to CVE-2021-1732

--- external/source/include/windows/common.h
+++ external/source/include/windows/common.h
@@ -27,6 +27,7 @@ static _inline void real_dprintf(char* format, ...)
 	strcat_s(buffer, sizeof(buffer), "\r\n");
 	OutputDebugStringA(buffer);
 	va_end(args);
+	Sleep(500);
 }
 
 typedef struct _EPROCESS_OFFSETS {
