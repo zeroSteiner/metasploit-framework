46aa332f9cbf403ccdf923ab4fe3009b4fea6b49
Spencer McIntyre <zeroSteiner@gmail.com>
2022-05-27 09:04:40 -0400

    Work in progress on LDAP add computer
    It is not working right now because the DC refuses to set the password
    over a plaintext connection. In ordre to connect with SSL it, it seems
    you need to have a cert from AD.

--- modules/auxiliary/admin/ldap/add_computer.rb
+++ modules/auxiliary/admin/ldap/add_computer.rb
@@ -38,12 +38,43 @@ class MetasploitModule < Msf::Auxiliary
     ])
   end
 
+  def random_hostname(prefix: 'DESKTOP')
+    "#{prefix}-#{Rex::Text::rand_base(8, '', ('A' .. 'Z').to_a + ('0' .. '9').to_a)}"
+  end
+
   def run
     print_status("#{peer} Connecting...")
     ldap_connect do |ldap|
-      fail_with(Failure::NoAccess, 'Failed to bind to the remote server.') unless ldap.get_operation_result.result == 0
+      unless ldap.get_operation_result.code == 0
+        fail_with(Failure::NoAccess, "Failed to bind to the remote server (#{ldap.get_operation_result.message})")
+      end
 
-      discover_base_dn(ldap)
+      hostname = random_hostname
+      base_dn = discover_base_dn(ldap)
+      computer_group = "CN=Computers,#{base_dn}"
+      computer_dn = "CN=#{hostname},#{computer_group}"
+
+      print_status("Adding: #{hostname}")
+
+      domain = 'MSFLAB.LOCAL'
+      password = Rex::Text::rand_text_alphanumeric(32)
+      spns = [
+        "HOST/#{hostname}",
+        "HOST/#{hostname}.#{domain}",
+        "RestrictedKrbHost/#{hostname}",
+        "RestrictedKrbHost/#{hostname}.#{domain}"
+      ]
+      ucd = {
+        'dnsHostName' => "#{hostname}.#{domain}",
+        # http://www.selfadsi.org/ads-attributes/user-userAccountControl.htm
+        'userAccountControl' => '4096',
+        'servicePrincipalName' => spns,
+        'sAMAccountName' => "#{hostname}$",
+        'unicodePwd' => "\"#{password}\"".encode('UTF-16LE').force_encoding('ASCII-8BIT'),
+        'objectClass' => ['top','person','organizationalPerson','user','computer']
+      }
+      result = ldap.add(dn: computer_dn, attributes: ucd)
+      print_status result.inspect
     end
   end
 end
