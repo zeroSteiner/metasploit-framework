4351de9213d91574449f1f21c33179fb8d2f090b
Spencer McIntyre <zeroSteiner@gmail.com>
2025-08-27 17:32:53 -0400

    Initial commit of larger PE templates

--- data/templates/src/pe/exe/template.c
+++ data/templates/src/pe/exe/template.c
@@ -1,11 +1,7 @@
-#include <stdio.h>
-
 #define SCSIZE 4096
 char payload[SCSIZE] = "PAYLOAD:";
 
-char comment[512] = "";
-
-int main(int argc, char **argv) {
+void main() {	
 	(*(void (*)()) payload)();
 	return(0);
 }
